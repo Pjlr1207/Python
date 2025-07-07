412d42d701f579cf42bdb14b460d2be485e8ea8b
Pjlr1207 <108364858+Pjlr1207@users.noreply.github.com>
2025-07-07 15:55:41 -0500

    comentarios agregados al api

--- app.py
+++ app.py
@@ -21,22 +21,25 @@ id_lista = 1 #Variable unica para id de frutas
 def fruit():
     return jsonify({'mensaje': 'Bienvenido al Api de frutas'})
 
-@app.route('/lista', methods=['GET'])
-def lista():
+#inicializando lista de frutas
+@app.route('/lista', methods=['GET']) #ruta para consultar la lista
+def lista(): #funcion lista
+    if not lista_frutas:
+        return jsonify({'mensaje': 'lista vacia, por favor agregue un producto'}) #verifica si esta vacia la lista
     return jsonify({'frutas': lista_frutas})
 
-@app.route ('/agregar/<nombre>', methods=['GET'])
-def agregar_fruta(nombre):
-    global id_lista
+@app.route ('/agregar/<nombre>', methods=['GET'])#ruta para agregar frutas
+def agregar_fruta(nombre): #define lafuncion para agregar frutas
+    global id_lista #define la variable global id_lista
     fruta={
         'id' : id_lista,
         'nombre': nombre
     }
-    lista_frutas.append(fruta)
+    lista_frutas.append(fruta) #agrega fruta a la lista
     
     mensaje= f'{nombre} agregada correctamente con el id {id_lista}'
     id_lista += 1
     return jsonify({'mensaje' : mensaje, })
 
-if __name__ == '__main__':
+if __name__ == '__main__': # ejecuta el archivo
     app.run (debug=False)
